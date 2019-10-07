a3c7ecffdbddf9bd303acf30c3b29ebfb9c1915b
Monica Djunaidi <modjun12@gmail.com>
2019-10-07 17:58:13 -0500

    wip

--- src/javascripts/components/eat/eat.js
+++ src/javascripts/components/eat/eat.js
@@ -5,24 +5,26 @@ let full = 100;
 
 const feed = (e) => {
   const buttonID = e.target.id;
-  if (buttonID === 'salad') {
-    full = Math.min(full + 10, 100);
-    utilities.printToDOM('max', full);
+  if (buttonID === 'dorayaki') {
+    full = Math.min((Math.random() * full) + 10, 100);
+    console.log(full);
+    document.getElementById('eatBar').value = full;
   } else if (buttonID === 'burger') {
-    full = Math.max(full - 3, 0);
-    utilities.printToDOM('max', full);
+    full = Math.max((Math.random() * full) - 3, 0);
+    console.log(full);
+    document.getElementById('eatBar').value = full;
   }
 };
 
 const eventListener = () => {
-  document.getElementById('salad').addEventListener('click', feed);
+  document.getElementById('dorayaki').addEventListener('click', feed);
   document.getElementById('burger').addEventListener('click', feed);
 };
 
 const eatButton = () => {
   let domString = '<h1>EAT</h1>';
-  domString += `<h2 id="max">${full}</h2>`;
-  domString += '<button id="salad" type="button">Salad</button>';
+  domString += `<progress max="100" value=${full} id="eatBar"></progress>`;
+  domString += '<button id="dorayaki" type="button">dorayaki</button>';
   domString += '<button id="burger">Burger</button>';
   utilities.printToDOM('eat', domString);
   eventListener();
--- src/javascripts/components/play/play.js
+++ src/javascripts/components/play/play.js
@@ -7,10 +7,10 @@ const playing = (e) => {
   const buttonID = e.target.id;
   if (buttonID === 'superFun') {
     fun = Math.min(fun + 50, 100);
-    utilities.printToDOM('playing', fun);
+    document.getElementById('playing').value = fun;
   } else if (buttonID === 'slightlyFun') {
     fun = Math.min(fun + 2, 100);
-    utilities.printToDOM('playing', fun);
+    document.getElementById('playing').value = fun;
   }
 };
 
@@ -21,7 +21,7 @@ const eventListener = () => {
 
 const playButton = () => {
   let domString = '<h1 class="playPosition">PLAY</h1>';
-  domString += `<h2 class="playPosition" id="playing">${fun}</h2>`;
+  domString += `<div class="playPosition"> <progress max="100" value=${fun} id="playing"></progress></div>`;
   domString += '<div class="playPosition"> <button id="superFun" type="button">Super Fun</button>';
   domString += '<button id="slightlyFun">Slightly Fun</button></div>';
   utilities.printToDOM('play', domString);
