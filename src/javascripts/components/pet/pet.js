1dbab6e03e12ed7bd12e4695c995fddd9acf4023
Monica Djunaidi <modjun12@gmail.com>
2019-12-21 14:11:15 -0600

    updatad doraemon image src

--- src/javascripts/components/pet/pet.js
+++ src/javascripts/components/pet/pet.js
@@ -2,7 +2,7 @@ import utilities from '../../helpers/utilities';
 import './pet.scss';
 
 const petPrint = () => {
-  const petPic = '<img src="/src/images/doraemon1.png">';
+  const petPic = '<img src="https://raw.githubusercontent.com/djunaim/tamagotchi/master/src/images/doraemon1.png">';
   utilities.printToDOM('pet', petPic);
 };
 
